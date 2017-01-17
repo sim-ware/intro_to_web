24858d9a342ec44bc4942ec5d6cc34b6d44ea2db
Samir Gossain <samir.gossain@gmail.com>
2017-01-17 12:52:12 +0000

    Sinatra:IntroducingParams

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-  get '/cat' do
-    @randomcat = ["Amigo", "Oscar", "Viking"].sample
+  get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
     erb(:index)
+  end
+
+  get '/named-cat' do
+    p params
+    @name = params[:name]
+    erb :index
   end
